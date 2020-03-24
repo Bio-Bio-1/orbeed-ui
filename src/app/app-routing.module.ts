a6c44d4c8450bcb23c3a721abd5f90322c8156d1
Md. Meftaul Haque Mishu <meftaulhaque@gmail.com>
2020-03-24 23:02:59 +0600

    Sidebar added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {OrbeedDashboardComponent} from './orbeed-dashboard/orbeed-dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: OrbeedDashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
